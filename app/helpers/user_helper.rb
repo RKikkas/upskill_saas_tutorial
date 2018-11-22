08f491cbcfa7ee7724de8579de0fe3d192333c64
Roger Kikkas <roger.kikkas@gmail.com>
2018-11-22 14:21:11 +0000

    Added profile update action

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -5,7 +5,7 @@ module UserHelper
     elsif @user.profile.job_title == "Entrepreneur"
       "<i class='fa fa-lightbulb-o'></i>".html_safe
     elsif @user.profile.job_title == "Investor"
-      "<i class='fa fa-dollar-sign'></i>".html_safe
+      "<i class='fa fa-dollar'></i>".html_safe
     end
   end
 end
